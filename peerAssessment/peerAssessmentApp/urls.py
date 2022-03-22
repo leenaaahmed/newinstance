5dfdacffe0f052795f1da38101503402ecb81886
laurensampson <sampsonl@bc.edu>
2022-03-22 14:20:57 -0400

    add in access code field

--- peerAssessment/peerAssessmentApp/urls.py
+++ peerAssessment/peerAssessmentApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'peerAssessmentApp'
 
 urlpatterns = [
+    path('', views.signup, name= 'home'),
     path('createuser', views.createUser, name='createuser'),
     path('home/',views.home_view, name= 'home'),
     path('signup/',views.signup, name='signup')
