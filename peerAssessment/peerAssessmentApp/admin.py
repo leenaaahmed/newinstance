dc81aba4b45388cdd97c855c50bed4bd0712258e
hannah <jiangbz@bc.edu>
2022-04-19 14:43:04 -0400

    add professor on course creations, add multiple admin to course, add student to course via access code on signup

--- peerAssessment/peerAssessmentApp/admin.py
+++ peerAssessment/peerAssessmentApp/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from .models import SiteUsers, Course, Registry, Enrollment, Team, Cassess, Question, Response, MCResponse
+from .models import SiteUsers, Course, Registry, Team, Question, Assessment, Answer, Submission, Choice
 
 # Register your models here.
 
 admin.site.register(SiteUsers)
 admin.site.register(Course)
 admin.site.register(Registry)
-admin.site.register(Enrollment)
+# admin.site.register(Enrollment)
 admin.site.register(Team)
-admin.site.register(Cassess)
+admin.site.register(Assessment)
 admin.site.register(Question)
-admin.site.register(Response)
-admin.site.register(MCResponse)
+admin.site.register(Answer)
+admin.site.register(Submission)
+admin.site.register(Choice)
+# admin.site.register(Cassess)
+# admin.site.register(Question)
+# admin.site.register(Response)
+# admin.site.register(MCResponse)
--- peerAssessment/peerAssessmentApp/forms.py
+++ peerAssessment/peerAssessmentApp/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 import datetime
 from django.forms import ModelForm
-from .models import Course, Registry, Cassess, Team, SiteUsers,Question,Response, MCResponse
+from .models import Course, Registry, Team, SiteUsers,Question, Answer, Assessment, Choice, Submission
 
 
 class SignUpForm(UserCreationForm):
@@ -13,81 +13,75 @@ class SignUpForm(UserCreationForm):
     access_code = forms.CharField(max_length = 5, help_text='Required')
     class Meta:
         model = User
-        fields = ('username','email', 'date_of_birth', 'password1', 'password2')
-
-class CourseCreation(forms.Form):
-    course = forms.CharField(max_length = 40, help_text = 'Description')
-    course_id = forms.CharField(max_length = 10, help_text = 'Required')
-    year = forms.CharField(max_length = 4, help_text = 'Required')
-    semester = forms.CharField(max_length = 1, help_text = 'S or F')
-    class Meta:
-        ##model = Course
-        fields = ('course', 'course_id', 'year', 'semester')
-
-#class GroupCreation(forms.Form):
- #   course = forms.CharField(max_length = 40, help_text = 'Required')
-  #  group_name = forms.CharField(max_length = 20, help_text = 'Required')
-   # class Meta:
-    #    fields = ('course', 'group_name')
-
+        fields = ('username','email', 'date_of_birth', 'password1', 'password2', 'access_code')
 class CourseForm(ModelForm):
     class Meta:
         model = Course
-        fields = ('course', 'course_id', 'year', 'semester')
+        fields = ('course_name', 'course_id', 'year', 'semester')
         labels = {
 
-            'course': '',
+            'course_name': '',
             'course_id':'',
             'year': '',
             'semester': '',
 
         }
+
+        YEAR_CHOICES = {
+            ("2021", "2021"),
+            ("2022", "2022"),
+            ("2023", "2023"),
+            ("2024", "2024"),
+        }
+        SEMESTER_CHOICES = {
+            ("Fall", "FALL"),
+            ("Spring", "SPRING"),
+        }
         widgets = {
-            'course': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Course Name'} ),
+            'course_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Course Name'} ),
             'course_id': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Course ID'} ),
-            'year': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Year'} ),
-            'semester': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Semester'} ),
+            'year': forms.Select(choices=YEAR_CHOICES, attrs={'class': 'form-control', 'placeholder': 'Year'} ),
+            'semester': forms.Select(choices=SEMESTER_CHOICES, attrs={'class': 'form-control', 'placeholder': 'Semester'}),
         }
 
 class RegistryForm(ModelForm):
     class Meta:
         model = Registry
-        fields = ('User','course')
+        fields = ('course_id','admin')
         labels = {
-
-            'User': '',
-            'course': '',
+            'course_id': '',
+            'admin': '',
         }
         widgets = {
-            'User': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Professor Name'} ),
-            'course': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Course Name'} ),
+            'course_id': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Course Name', 'help_text': 'Course ID'} ),
+            #'admin': forms.SelectMultiple(attrs={'class': 'form-control', 'required': 'true', 'help_text': 'Admin'} ),
+            'admin': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Select Admin'} ),
         }
 
-class CassessForm(ModelForm):
-    class Meta:
-        model = Cassess
-        fields = ('assess_number', 'due_date', 'publish_date', 'course')
-        labels = {
-            'assess_number': '',
-            'due_date': '',
-            'publish_date': '',
-            'course': '',
-        }
-        widgets = {
-            'assess_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Assessment Number'}),
-            'due_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Due Date'}),
-            'publish_date': forms.TextInput(attrs={'class': 'forms-control','placeholder': 'Publish Date'}),
-            'course': forms.Select(attrs={'class': 'forms-control','placeholder': 'Course'}),
-        }
+# class CassessForm(ModelForm):
+#     class Meta:
+#         model = Cassess
+#         fields = ('assess_number', 'due_date', 'publish_date', 'course')
+#         labels = {
+#             'assess_number': '',
+#             'due_date': '',
+#             'publish_date': '',
+#             'course': '',
+#         }
+#         widgets = {
+#             'assess_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Assessment Number'}),
+#             'due_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Due Date'}),
+#             'publish_date': forms.TextInput(attrs={'class': 'forms-control','placeholder': 'Publish Date'}),
+#             'course': forms.Select(attrs={'class': 'forms-control','placeholder': 'Course'}),
+#         }
 
 class TeamForm(ModelForm):
     class Meta:
         model = Team
         fields = ('team_name', 'course', 'memebers')
         labels = {
-
-            'team_name': '', 
-            'course': '', 
+            'team_name': '',
+            'course': '',
             'memebers': '',
         }
         widgets = {
@@ -97,60 +91,57 @@ class TeamForm(ModelForm):
 
         }
 
-class QuestionForm(ModelForm):
-    class Meta:
-        model = Question
-        fields = ('assessment', 'question')
-        labels = {
+# class QuestionForm(ModelForm):
+#     class Meta:
+#         model = Question
+#         fields = ('assessment', 'question')
+#         labels = {
 
-            'assessment': '', 
-            'question': '', 
-            
-        }
-        widgets = {
-            'assessment': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Assessment'} ),
-            'question': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Question'} ),
-        }
+#             'assessment': '',
+#             'question': '',
 
-class ResponseForm(ModelForm):
-    class Meta:
-        model = Response
-        fields = ('response', 'question')
-        labels = {
- 
-            'response': '', 
-            'question': '',
-            
-            
-        }
-        widgets = {
-            'response': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Response'} ),
-            'question': forms.Select(attrs={'class': 'form-control', 'placeholder': 'question'} ),
-            
-         } 
+#         }
+#         widgets = {
+#             'assessment': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Assessment'} ),
+#             'question': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Question'} ),
+#         }
 
-class MCResponseForm(ModelForm):
-    class Meta:
-        model = MCResponse
+# class ResponseForm(ModelForm):
+#     class Meta:
+#         model = Response
+#         fields = ('response', 'question')
+#         labels = {
 
-        fields = ('mc', 'question')
-        labels = {
+#             'response': '',
+#             'question': '',
 
-            'mc': '', 
-            'question': '',
-            
-        }
-        widgets = {
-            'mc': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Multiple Choice Response'} ),
-            'question': forms.Select(attrs={'class': 'form-control', 'placeholder': 'question'} ),
 
-         } 
+#         }
+#         widgets = {
+#             'response': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Response'} ),
+#             'question': forms.Select(attrs={'class': 'form-control', 'placeholder': 'question'} ),
+
+#          }
+
+# class MCResponseForm(ModelForm):
+#     class Meta:
+#         model = MCResponse
+
+#         fields = ('mc', 'question')
+#         labels = {
+
+#             'mc': '',
+#             'question': '',
+
+#         }
+#         widgets = {
+#             'mc': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Multiple Choice Response'} ),
+#             'question': forms.Select(attrs={'class': 'form-control', 'placeholder': 'question'} ),
+
+#          }
 
 class ContactForm(forms.Form):
 	first_name = forms.CharField(max_length = 50)
 	last_name = forms.CharField(max_length = 50)
 	email_address = forms.EmailField(max_length = 150)
 	message = forms.CharField(widget = forms.Textarea, max_length = 2000)
-
-
-
