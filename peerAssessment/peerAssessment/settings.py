b6cf3d313d2aabf4fc73f8f931c79c57e997584c
jackbomba <JACKBOMBA2@GMAIL.COM>
2022-05-04 23:03:37 -0400

    deleted stuff so app works

--- peerAssessment/peerAssessment/settings.py
+++ peerAssessment/peerAssessment/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -137,11 +137,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'newinstanceco@gmail.com'
 EMAIL_HOST_PASSWORD = 'wordle19'
 
-DEBUG = False
-ALLOWED_HOSTS ['localhost', '127.0.0.1', 'newinstance.herokuapp.com',]
 
-#Active Django-Heroku.
-django_heroku.settings(locals())
 
 
 
